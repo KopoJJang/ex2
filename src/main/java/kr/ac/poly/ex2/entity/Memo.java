05516715013e1f6b663d0e8497b8d0dcb9106e1f
KopoJJang <2201110369@office.kopo.ac.kr>
2023-03-28 16:29:49 +0900

    Query 메소드와 @Query(JPQL)

--- src/main/java/kr/ac/poly/ex2/entity/Memo.java
+++ src/main/java/kr/ac/poly/ex2/entity/Memo.java
@@ -1,2 +1,22 @@
-package kr.ac.poly.ex2.entity;public class Memo {
+package kr.ac.poly.ex2.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tble_memo")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+
 }
